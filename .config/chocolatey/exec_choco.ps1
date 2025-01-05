6227d0c20da67e9da9d9aa45ad77ab35e0bc2319
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-01-05 16:26:45 +0000

    Confirm cause of failing: part 4.

--- .config/chocolatey/exec_choco.ps1
+++ .config/chocolatey/exec_choco.ps1
@@ -2,4 +2,4 @@ $currentDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 PowerShell -ExecutionPolicy Unrestricted $currentDir\init.ps1
 PowerShell -ExecutionPolicy Unrestricted $currentDir\install.ps1
 # list installed packages
-choco list -lo
+choco list
