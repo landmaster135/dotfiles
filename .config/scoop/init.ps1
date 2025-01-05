eda97df73bea1c5e067f57ddd1a8f7cd34748057
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-01-05 12:57:25 +0000

    Fix installation path.

--- .config/scoop/init.ps1
+++ .config/scoop/init.ps1
@@ -1,6 +1,12 @@
-cd C:\
+Set-Location "C:\"
+$firstDir = (Get-Location).Path
+Set-Location "D:\"
+Get-Location
+
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+Set-Location $firstDir
+
 $username = (Get-ChildItem Env:USERNAME).Value;
-$targetDir = Read-Host "Input the directory you wanna install packages under your username.";
-Set-Location "C:\Users\$username\$targetDir";
+# $targetDir = Read-Host "Input the directory you wanna install packages under your username.";
+# Set-Location "C:\Users\$username\$targetDir";
