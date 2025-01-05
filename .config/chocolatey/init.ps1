ad09dc0450960b5657420d6e6f325d51ac51ea27
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-01-05 16:00:25 +0000

    Confirm cause of failing: part 3.

--- .config/chocolatey/init.ps1
+++ .config/chocolatey/init.ps1
@@ -1,3 +1,12 @@
-cd C:\
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+$firstDir = (Get-Location).Path
+# Set-Location "D:\"
+Set-Location "C:\"
+Get-Location
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-Location $firstDir
+
+$username = (Get-ChildItem Env:USERNAME).Value;
+# $targetDir = Read-Host "Input the directory you wanna install packages under your username.";
+# Set-Location "C:\Users\$username\$targetDir";
