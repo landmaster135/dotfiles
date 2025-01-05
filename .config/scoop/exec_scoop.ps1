455ebaebfc645219037f648ec502dcf2ec251d5a
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-01-05 12:36:27 +0000

    Fix path to install scoop packages.

--- .config/scoop/exec_scoop.ps1
+++ .config/scoop/exec_scoop.ps1
@@ -1,5 +1,6 @@
-PowerShell -ExecutionPolicy Unrestricted .\init.ps1
-PowerShell -ExecutionPolicy Unrestricted .\install_with_main.ps1
-PowerShell -ExecutionPolicy Unrestricted .\install_with_extras.ps1
-PowerShell -ExecutionPolicy Unrestricted .\install_with_games.ps1
+$currentDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+PowerShell -ExecutionPolicy Unrestricted $currentDir\init.ps1
+PowerShell -ExecutionPolicy Unrestricted $currentDir\install_with_main.ps1
+PowerShell -ExecutionPolicy Unrestricted $currentDir\install_with_extras.ps1
+PowerShell -ExecutionPolicy Unrestricted $currentDir\install_with_games.ps1
 scoop list
