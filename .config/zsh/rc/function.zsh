ecb89589c28fc843a5b913883e6d299b3bcfee45
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-01-13 00:53:20 +0000

    Add aliases and functions.

--- .config/zsh/rc/function.zsh
+++ .config/zsh/rc/function.zsh
@@ -9,3 +9,8 @@ function du-ah() {
   # e.g. du-ah / 20
   du -ah $1 | sort -rh | head -n $2
 }
+
+function git-erase() {
+  # e.g. git-erase credential.json
+  git filter-branch --force --index-filter "git rm --cached --ignore-unmatch $1" -- --all
+}
