a654a6bf638483b5df12e8a13ff9b82976102b72
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-01-29 21:04:34 +0000

    Fix function to show snippets and to test that

--- .config/zsh/rc/function.zsh
+++ .config/zsh/rc/function.zsh
@@ -48,34 +48,76 @@ function snippet() {
     echo "$input_string" | sed "s/$remove_str//g"
   }
 
+  function here_are_the_available_snippets() {
+    echo -e ""
+    echo "#################################################################"
+    echo "######   Here are the available snippets for $1.   ######"
+    echo "#################################################################"
+    echo -e ""
+  }
+
   while [ $# -gt 0 ]; do
     docker_array=("aliases" "build-options" "options" "run-options" "subcommands")
+    git_array=("diff-options" "options" "subcommands")
+    psql_array=("commands")
+    tmux_array=("options" "subcommands")
     case ${1} in
       --help | -h)
         docker_array=($(add_prefix "--" "${docker_array[@]}"))
-        echo -e ""
+        git_array=($(add_prefix "--" "${git_array[@]}"))
+        psql_array=($(add_prefix "--" "${psql_array[@]}"))
+        tmux_array=($(add_prefix "--" "${tmux_array[@]}"))
         echo -e "Usage: ${BASH_SOURCE[0]:-$0} [docker | git | psql | tmux] [--help | -h]" 0>&2
-        echo "  docker: show snippets for docker with the following options. [$(echo "${docker_array[*]}" | tr ' ' '|' | sed 's/|/ | /g')]"
-        echo -e "  git: show snippets for git with the following options. [--diff-options | --options | --subcommands]"
-        echo -e "  psql: show snippets for psql with the following options. [--commands]"
-        echo -e "  tmux: show snippets for tmux with the following options. [--options | --subcommands]"
-        echo -e ""
-        exit 1
+        echo -e "  docker: show snippets for docker with the following options. [$(echo "${docker_array[*]}" | tr ' ' '|' | sed 's/|/ | /g')]"
+        echo -e "  git: show snippets for git with the following options. [$(echo "${git_array[*]}" | tr ' ' '|' | sed 's/|/ | /g')]"
+        echo -e "  psql: show snippets for psql with the following options. [$(echo "${psql_array[*]}" | tr ' ' '|' | sed 's/|/ | /g')]"
+        echo -e "  tmux: show snippets for tmux with the following options. [$(echo "${tmux_array[*]}" | tr ' ' '|' | sed 's/|/ | /g')]"
+				# exit 1
 				;;
       docker)
         snippet_file=$(remove_substring_sed $2 "--")
         if contains_element $(remove_substring_sed $2 "--") "${docker_array[@]}"; then
+          here_are_the_available_snippets "Docker"
           cat $ZHOMEDIR/snippets/docker/$snippet_file.txt
         fi
         ;;
-      # --help | -h)
-			# 	;;
+      git)
+        snippet_file=$(remove_substring_sed $2 "--")
+        if contains_element $(remove_substring_sed $2 "--") "${git_array[@]}"; then
+          here_are_the_available_snippets "Git"
+          cat $ZHOMEDIR/snippets/git/$snippet_file.txt
+        fi
+        ;;
+      psql)
+        snippet_file=$(remove_substring_sed $2 "--")
+        if contains_element $(remove_substring_sed $2 "--") "${psql_array[@]}"; then
+          here_are_the_available_snippets "PostgreSQL"
+          cat $ZHOMEDIR/snippets/postgresql/$snippet_file.txt
+        fi
+        ;;
+      tmux)
+        snippet_file=$(remove_substring_sed $2 "--")
+        if contains_element $(remove_substring_sed $2 "--") "${tmux_array[@]}"; then
+          here_are_the_available_snippets "Docker"
+          echo "Here are the available snippets for Tmux."
+          cat $ZHOMEDIR/snippets/tmux/$snippet_file.txt
+        fi
+        ;;
+      --aliases | --build-options | --options | --run-options | --subcommands | --diff-options | --commands)
+				;;
+      general)
+          here_are_the_available_snippets "general"
+        cat $ZHOMEDIR/snippets/general/general.txt
+        ;;
+      --aliases | --build-options | --options | --run-options | --subcommands | --diff-options | --commands)
+				;;
       *)
 				echo "[ERROR] Invalid arguments '${1}'"
 				usage
-				exit 1
+				# exit 1
 				;;
     esac
+    echo -e ""
 		shift
 	done
 }
---
+++ install_scripts/test_after_installed.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#--------------------------------------------------------------#
+##          Snippets                                          ##
+#--------------------------------------------------------------#
+
+snippet --help
+snippet -h
+snippet docker --aliases
+snippet docker --build-options
+snippet docker --options
+snippet docker --run-options
+snippet docker --subcommands
+snippet git --diff-options
+snippet git --options
+snippet git --subcommands
+snippet psql --commands
+snippet tmux --options
+snippet tmux --subcommands
+snippet general
