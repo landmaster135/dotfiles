b3e7bd23ed3bf91585c9abb54b1cf0e90474899b
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-02-03 02:49:41 +0000

    Add apt snippets.

--- .config/zsh/rc/function.zsh
+++ .config/zsh/rc/function.zsh
@@ -50,70 +50,86 @@ function snippet() {
 
   function here_are_the_available_snippets() {
     echo -e ""
-    echo "#################################################################"
+    echo "####################################################################"
     echo "######   Here are the available snippets for $1.   ######"
-    echo "#################################################################"
+    echo "####################################################################"
     echo -e ""
   }
 
   while [ $# -gt 0 ]; do
+    apt_array=("commands-for-disk")
     docker_array=("aliases" "build-options" "options" "run-options" "subcommands")
     git_array=("diff-options" "options" "subcommands")
     psql_array=("commands")
     tmux_array=("options" "subcommands")
     case ${1} in
       --help | -h)
+        apt_array=($(add_prefix "--" "${apt_array[@]}"))
         docker_array=($(add_prefix "--" "${docker_array[@]}"))
         git_array=($(add_prefix "--" "${git_array[@]}"))
         psql_array=($(add_prefix "--" "${psql_array[@]}"))
         tmux_array=($(add_prefix "--" "${tmux_array[@]}"))
-        echo -e "Usage: ${BASH_SOURCE[0]:-$0} [docker | git | psql | tmux] [--help | -h]" 0>&2
+        echo -e "Usage: ${BASH_SOURCE[0]:-$0} [common | apt | docker | git | psql | tmux] [--help | -h]" 0>&2
         echo -e "  common: show common snippets."
+        echo -e "  apt: show snippets for Debian/Ubuntu terminals with the following options. [$(echo "${apt_array[*]}" | tr ' ' '|' | sed 's/|/ | /g')]"
         echo -e "  docker: show snippets for docker with the following options. [$(echo "${docker_array[*]}" | tr ' ' '|' | sed 's/|/ | /g')]"
         echo -e "  git: show snippets for git with the following options. [$(echo "${git_array[*]}" | tr ' ' '|' | sed 's/|/ | /g')]"
         echo -e "  psql: show snippets for psql with the following options. [$(echo "${psql_array[*]}" | tr ' ' '|' | sed 's/|/ | /g')]"
         echo -e "  tmux: show snippets for tmux with the following options. [$(echo "${tmux_array[*]}" | tr ' ' '|' | sed 's/|/ | /g')]"
 				# exit 1
 				;;
+      apt)
+        snippet_file=$(remove_substring_sed $2 "--")
+        if contains_element $(remove_substring_sed $2 "--") "${apt_array[@]}"; then
+          here_are_the_available_snippets "Debian/Ubuntu"
+          cat $ZHOMEDIR/snippets/$1/$snippet_file.txt
+        fi
+        return 0
+        ;;
       common)
           here_are_the_available_snippets "common"
-        cat $ZHOMEDIR/snippets/common/common.txt
+        cat $ZHOMEDIR/snippets/$1/common.txt
         ;;
       docker)
         snippet_file=$(remove_substring_sed $2 "--")
         if contains_element $(remove_substring_sed $2 "--") "${docker_array[@]}"; then
           here_are_the_available_snippets "Docker"
-          cat $ZHOMEDIR/snippets/docker/$snippet_file.txt
+          cat $ZHOMEDIR/snippets/$1/$snippet_file.txt
         fi
+        return 0
         ;;
       git)
         snippet_file=$(remove_substring_sed $2 "--")
         if contains_element $(remove_substring_sed $2 "--") "${git_array[@]}"; then
           here_are_the_available_snippets "Git"
-          cat $ZHOMEDIR/snippets/git/$snippet_file.txt
+          cat $ZHOMEDIR/snippets/$1/$snippet_file.txt
         fi
+        return 0
         ;;
       psql)
         snippet_file=$(remove_substring_sed $2 "--")
         if contains_element $(remove_substring_sed $2 "--") "${psql_array[@]}"; then
           here_are_the_available_snippets "PostgreSQL"
-          cat $ZHOMEDIR/snippets/postgresql/$snippet_file.txt
+          cat $ZHOMEDIR/snippets/$1/$snippet_file.txt
         fi
+        return 0
         ;;
       tmux)
         snippet_file=$(remove_substring_sed $2 "--")
         if contains_element $(remove_substring_sed $2 "--") "${tmux_array[@]}"; then
           here_are_the_available_snippets "Docker"
           echo "Here are the available snippets for Tmux."
-          cat $ZHOMEDIR/snippets/tmux/$snippet_file.txt
+          cat $ZHOMEDIR/snippets/$1/$snippet_file.txt
         fi
+        return 0
         ;;
-      --aliases | --build-options | --options | --run-options | --subcommands | --diff-options | --commands)
+      --aliases | --build-options | --commands-for-disk | --options | --run-options | --subcommands | --diff-options | --commands)
+        return 1
 				;;
       *)
 				echo "[ERROR] Invalid arguments '${1}'"
 				usage
-				# exit 1
+        return 1
 				;;
     esac
     echo -e ""
