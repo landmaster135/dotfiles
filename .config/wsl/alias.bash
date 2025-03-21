0e878c1b300838ec8217ced73c386b4da8e5450d
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2025-03-22 06:05:32 +0900

    Apply settings for WSL

---
+++ .config/wsl/alias.bash
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+#==============================================================#
+##          Common aliases                                    ##
+#==============================================================#
+
+# common
+# alias tree="pwd;find . | sort | sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/| /g'"
+alias nano='nano -m'
+alias mv='mv -i'
+alias cp='cp -irf'
+alias cl='clear'
+# alias wc-l='awk '\''END { print NR, FILENAME }'\'''
+alias fc='fc -r'
+# alias ..='cd ..'
+alias ps-fa='ps -fA'
+
+# common custom function
+alias getpids='getpids'
+alias cron_help='cron_help'
+alias compare_json='compare_json'
+
+# ls
+alias ls='ls --color=auto'
+alias la='ls -AF --color=auto'
+alias l1='ls -1A --color=auto'
+alias lal='ls -alF --color=auto'
+alias lsoi='lsof -i'
+
+# grep
+alias grep='grep -H -n -I -i --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias histgrep='history | grep'
+alias ps-grep='ps -fA | grep -e UID -e'
+alias apt-grep='apt list --installed | grep -e'
+alias ali-grep='alias | grep -e'
+
+# disk
+alias df='df -h'
+alias du-sh='du -sh'
+alias du-ah='du-ah ~ 20'
+alias apt-clean='apt-get clean' # Clear cache of apt packages. (But that access is denied in Cloud Shell due to insufficient permissions.)
+alias rm-auto='apt-get autoremove' # Remove unnecessary packages that have no dependencies.
+alias rm-log='sudo journalctl --vacuum-time=2weeks' # Remove unnecessary log files. System log files are retained for an extended period.
+alias emptrash='rm -rf ~/.local/share/Trash/*' # Empty rubbish bin.
+alias rm-cache='rm -rf ~/.cache/*'
+
+# git
+alias git-ba='git branch -a'
+alias git-a='git add .'
+alias git-c='git commit -m'
+alias git-publish='BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD); git push --set-upstream origin "$BRANCH_NAME"; unset BRANCH_NAME;'
+alias git-push-afo='git push --all --force origin'
+alias git-res='git reset --soft HEAD^'
+alias git-reh='git reset --hard HEAD^'
+alias git-erase='git-erase'
+alias git-repat='git-repat'
+alias git-nb='git-nb'
+
+# chmod
+alias 644='chmod 644'
+alias 755='chmod 755'
+alias 777='chmod 777'
+alias scown='sudo chown -c -R $USER:$USER $HOME'
+
+# snippet
+alias sni-co='snippet common;'
+alias sni-apt='snippet apt --commands-for-disk;'
+alias sni-do='snippet docker --aliases; snippet docker --build-options; snippet docker --options; snippet docker --run-options; snippet docker --subcommands;'
+alias sni-gi='snippet git --diff-options; snippet git --options; snippet git --subcommands;'
+alias sni-go='snippet go --options; snippet go --subcommands;'
+alias sni-ps='snippet psql --commands;'
+alias sni-tm='snippet tmux --options; snippet tmux --subcommands;'
+
+#==============================================================#
+##          Python aliases                                    ##
+#==============================================================#
+
+# pip
+alias pip-i='pip install --no-cache-dir'
+alias pip-ip='pip install --no-cache-dir -r requirements.txt'
+alias pip-id='pip install --no-cache-dir -r requirements/dev.txt'
+alias pip-vu='pip -V; sudo python3 -m pip install --upgrade pip'
+alias pip-l='pip list'
+alias pip-grep='pip list | grep -i'
+
+# virtual env
+alias py-ve='python -m venv .venv'
+alias py-va='source .venv/bin/activate; edit-ps1-env -a;'
+alias py-vd='deactivate; edit-ps1-env -d;'
+alias py-vi='py-ve; py-va; pip-id;'
+alias rm-pyc='rm -rf __pycache__/; rm -rf .pytest_cache/; rm -rf src/__pycache__/; rm -rf tests/__pycache__/; py-vd; rm -rf .venv/;'
+
+# uvicorn
+alias uvi='uvicorn src.app:app --reload --host 0.0.0.0 --port 8080'
+alias uvi-kill='sudo lsof -t -i tcp:8080 | xargs kill -9'
+
+# python
+alias py='python'
+alias py-sa='python ./src/app.py'
+alias py-sm='python ./src/main.py'
+
+# pytest
+alias pyt-d='python -m pytest --durations=0 --tb=short'
+alias pyt-c='python -m pytest --cov=src --cov-branch --tb=short'
+alias pyt-v='python -m pytest --cov=src --cov-branch --tb=short -vv'
+alias pyt-vs='python -m pytest --cov=src --cov-branch --tb=short -vv -s'
+alias pyt-r='python -m pytest -n auto --cov=src --cov-branch --cov-report=html --tb=short'
+
+#==============================================================#
+##          PostgreSQL aliases                                ##
+#==============================================================#
+
+# setting
+alias apt-ja='sudo apt-get install language-pack-ja -y'
+
+# postgresql
+alias pg-re='sudo /sbin/service postgresql restart'
+alias pg-start='sudo /sbin/service postgresql start'
+alias pg-stop='sudo /sbin/service postgresql stop'
+alias su-pg='sudo su - postgres'
+alias pg-status='sudo /sbin/service postgresql status'
+
+#==============================================================#
+##          SQLite aliases                                    ##
+#==============================================================#
+
+alias sq3='sqlite3'
+
+#==============================================================#
+##          Gcloud aliases                                    ##
+#==============================================================#
+
+# common
+alias gc='gcloud'
+alias gc-fd='gcloud functions deploy'
+alias gc-sjc='gcloud scheduler jobs create'
+alias gc-init='gcloud init'
+alias gc-initco='gcloud init --console-only'
+alias gc-auth='gcloud auth application-default login'
+
+# cloud shell editor
+# alias teachme='cloudshell launch-tutorial'
+
+#==============================================================#
+##          Go aliases                                        ##
+#==============================================================#
+
+# Go
+alias go-l='go list -m -u all'
+alias go-mi='go mod init'
+alias go-mt='go mod tidy'
+alias go-r='go run -trimpath'
+alias go-t='go test -v ./... -coverpkg=./mypkg -covermode=count'
+alias go-b='go build -o'
+alias go-bl='GOOS=linux GOARCH=amd64 go build -o'
+alias go-bw='GOOS=windows GOARCH=amd64 go build -o'
+alias go-predeploy='cd mypkg; go mod init a.b/mypkg; go mod tidy; cd ..'
+alias gofmt-all='gofmt -w ./...'
+alias go-cn='go clean -i -n'
+alias go-c='go clean -i'
+alias clean_go_pkg='clean_go_pkg'
--- README.md
+++ README.md
@@ -35,12 +35,28 @@ exec zsh
 4. Enjoy!
 
 ## Small install
-Only aliases and functions.
+Only aliases and functions. (CRLF break code)
 ```bash
 export ZHOMEDIR=$HOME/dotfiles/.config/zsh
 export ZRCDIR=$ZHOMEDIR/rc
 source "$ZRCDIR/alias.zsh"
 source "$ZRCDIR/function.zsh"
+
+export PS1='$(_append_history_line)\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[\033[01;33m\]$(_current_branch)\[\033[00m\]\[\033[01;35m\]\$\[\033[00m\] '
+export MY_PS1='$(_append_history_line)\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[\033[01;33m\]$(_current_branch)\[\033[00m\]\[\033[01;35m\]\$\[\033[00m\] '
+
+HISTCONTROL=erasedups
+
+```
+
+## WSL
+Only aliases and functions. (LF break code)
+```bash
+export ZHOMEDIR=$HOME/dotfiles/.config/zsh
+export ZRCDIR=$ZHOMEDIR
+export WSLHOMEDIR=$HOME/dotfiles/.config/wsl
+source "$WSLHOMEDIR/alias.bash"
+source "$WSLHOMEDIR/function.bash"
 
 export PS1='$(_append_history_line)\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[\033[01;33m\]$(_current_branch)\[\033[00m\]\[\033[01;35m\]\$\[\033[00m\] '
 export MY_PS1='$(_append_history_line)\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[\033[01;33m\]$(_current_branch)\[\033[00m\]\[\033[01;35m\]\$\[\033[00m\] '
