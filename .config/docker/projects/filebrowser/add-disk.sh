5c25d3baaed6fbb57e7d1b07670b61273d9f4af6
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2026-03-08 04:11:51 +0900

    feat: add mounted disks into docker compose and mounting scripts

--- .config/docker/projects/filebrowser/add-disk.sh
+++ .config/docker/projects/filebrowser/add-disk.sh
@@ -11,8 +11,17 @@ sudo mkfs.exfat /dev/sdb2
 
 # HDDをマウント
 mkdir /mnt/hdd01
-sudo mount -t exfat -o uid=1000,gid=1000,umask=000 /dev/sdb2 /mnt/hdd01
-# 保存して設定を確認
+# sudo mount -t exfat -o uid=1000,gid=1000,umask=000 /dev/sdb2 /mnt/hdd01
+
+# 自動マウント対象に追加
+sudo nano /etc/fstab
+# 内容としては下記となる（UUIDは実際の値に置き換える）
+cat <<EOF >> /etc/fstab
+UUID=1234-5678  /mnt/hdd01  exfat  uid=1000,gid=1000,umask=000  0  0
+EOF
+# 保存
 sudo mount -a
+# 設定を確認
+df -h | grep hdd01
 
 echo "Setup complete."
